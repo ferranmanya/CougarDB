8ab97db16e91b2fb261b07c4945e158313a1a94f
Alejandro Cano <alex.cano1994@gmail.com>
2019-09-26 20:04:56 +0200

    Inicializacion del proyecto

---
+++ src/main/java/cougardb/documentdb/DocumentdbApplication.java
@@ -0,0 +1,13 @@
+package cougardb.documentdb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DocumentdbApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DocumentdbApplication.class, args);
+    }
+
+}
