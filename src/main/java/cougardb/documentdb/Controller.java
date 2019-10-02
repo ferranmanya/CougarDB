188bb5da3bd06d4e6a403ca6488ee2949ec6283a
Ferran Manyà <ferranmanya@outlook.com>
2019-10-02 23:43:40 +0200

    Metadata

--- src/main/java/cougardb/documentdb/Controller.java
+++ src/main/java/cougardb/documentdb/Controller.java
@@ -1,35 +1,43 @@
 package cougardb.documentdb;
 import com.google.gson.Gson;
+import com.google.gson.reflect.TypeToken;
 
 import java.io.*;
+import java.lang.reflect.Type;
 import java.util.*;
 
 public class Controller {
-        public static final String METADATA = "metadata.json";
-        
-        public void CreateCollection(String CollectionName) throws CollectionAlreadyExistsException, IOException {
-            // 0.BUSCAR SI EXISTE YA EL CollectionName EN EL FICHERO METADATA  --El caso de error se lanzaria un CollectionAlreadyExistsException
-                //Dejo el stream abierto ya que escribire en metadata.json al final del metodo
-            
-            Gson gson = new Gson();
 
-            String pathname = CollectionName + ".cdb";
-            java.util.Date date = new Date();
-            CougarCollection newCollection = new CougarCollection(CollectionName, date, new File(pathname));
+    private static final String METADATA = "metadata.json";
 
-            this.writeCollection(newCollection, new ArrayList<HashMap<String, Object>>());
-
-            // 3.ESCRIBIR el objeto CougarCollection en el fichero METADATA
-            
-
+    public void CreateCollection(String CollectionName) throws CollectionAlreadyExistsException, IOException {
+        CougarCollection newCollection = new CougarCollection(CollectionName);
+        ArrayList<CougarCollection> collections = this.readMetadata();
+        if(collections.contains(newCollection)){
+            throw new CollectionAlreadyExistsException("");
         }
+        this.writeCollection(newCollection, new ArrayList<HashMap<String, Object>>());
+        this.writeMetadata(collections);
+    }
 
-        public void writeCollection(CougarCollection cougarCollection, ArrayList<HashMap<String, Object>> data) throws IOException {
-            FileOutputStream fos= new FileOutputStream(cougarCollection.getPath());
-            ObjectOutputStream os = new ObjectOutputStream (fos);
-            os.writeObject(data);
-            fos.close();
-            os.close();
-        }
+    private void writeMetadata(ArrayList<CougarCollection> collections) throws IOException {
+        Gson gson = new Gson();
+        gson.toJson(collections, new FileWriter(METADATA));
+    }
+
+
+    private ArrayList<CougarCollection> readMetadata() throws FileNotFoundException {
+        Gson gson = new Gson();
+        Type collectionList = new TypeToken<ArrayList<CougarCollection>>(){}.getType();
+        return gson.fromJson(new FileReader(METADATA), collectionList);
+    }
+
+    private void writeCollection(CougarCollection cougarCollection, ArrayList<HashMap<String, Object>> data) throws IOException {
+        FileOutputStream fos= new FileOutputStream(cougarCollection.getPath());
+        ObjectOutputStream os = new ObjectOutputStream (fos);
+        os.writeObject(data);
+        fos.close();
+        os.close();
+    }
 
 }
--- src/main/java/cougardb/documentdb/DocumentdbApplication.java
+++ src/main/java/cougardb/documentdb/DocumentdbApplication.java
@@ -1,13 +1,28 @@
 package cougardb.documentdb;
 
+import com.google.gson.Gson;
+import com.google.gson.reflect.TypeToken;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import java.io.FileReader;
+import java.lang.reflect.Type;
+import java.util.ArrayList;
 
 @SpringBootApplication
 public class DocumentdbApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(DocumentdbApplication.class, args);
+
+        Gson gson = new Gson();
+        ArrayList<CougarCollection> ccs = new ArrayList<CougarCollection>();
+        ccs.add(new CougarCollection("AAA"));
+        ccs.add(new CougarCollection("BBB"));
+
+        Type collectionList = new TypeToken<ArrayList<CougarCollection>>(){}.getType();
+        ccs = gson.fromJson(gson.toJson(ccs), collectionList);
+        System.out.println(gson.toJson(ccs));
     }
 
 }
