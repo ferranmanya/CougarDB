9fcf57e996a07f2b54667865199eef6b6d45d047
DavidAlvarezRios <dalvarezrios@gmail.com>
2019-12-19 19:32:06 +0100

    rama auxiliar por si acaso

--- src/main/java/cougardb/documentdb/model/CougarCollection.java
+++ src/main/java/cougardb/documentdb/model/CougarCollection.java
@@ -3,12 +3,13 @@ package cougardb.documentdb.model;
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import com.fasterxml.jackson.databind.ObjectMapper;
 
+import javax.annotation.PreDestroy;
 import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
 import java.util.*;
 import java.io.*;
 
-@JsonIgnoreProperties(value = { "mapper", "blocks" })
+@JsonIgnoreProperties(value = { "mapper", "blocks", "indexManager", "index" })
 public class CougarCollection {
 
     private String collectionName;
@@ -17,6 +18,8 @@ public class CougarCollection {
     private double maxFileSize; // kb
     private int currentId;
     private ObjectMapper mapper = new ObjectMapper();
+    //private TreeMap<UUID, Integer> index = new TreeMap<>();
+    private IndexManager indexManager;
 
     public CougarCollection(){}
 
@@ -25,9 +28,12 @@ public class CougarCollection {
         this.creationDate = new Date();
         this.currentId = 0;
         this.maxFileSize = 1.;
+        this.indexManager = new IndexManager(this.collectionName);
+        //this.indexManager = new IndexManager(this.collectionName);
+        //this.index = new TreeMap<>();
     }
 
-    public synchronized void readFileBlocks(boolean readData){
+    public void readFileBlocks(boolean readData){
         this.blocks = new ArrayList<>();
         for (int i = 0; i <= this.currentId; i++) {
             CollectionBlock block = new CollectionBlock(this.collectionName, i, this.maxFileSize);
@@ -39,11 +45,15 @@ public class CougarCollection {
     }
 
     public boolean putData(Map<String, Object> data, String id){
+        UUID final_id = UUID.randomUUID();
+
         if(id.length() != 0){
+            final_id = UUID.fromString(id);
             data.put("id", id);
+            //System.out.println(id);
         }
         else{
-            data.put("id", UUID.randomUUID());
+            data.put("id", final_id);
         }
         readFileBlocks(false); // load block list into memory
         //TODO escriure sempre a l'última pàgina (opc. defragmentacio, repaginació whatevs)
@@ -52,18 +62,20 @@ public class CougarCollection {
             final int dataLength = json.getBytes(StandardCharsets.UTF_8).length; // calculate the length of data
             Optional<CollectionBlock> result = blocks.stream().filter(block -> block.getFile().length() + dataLength < maxFileSize*1024).findFirst();
             if(result.isPresent()){ // in case there is a block with enough available space, we insert it there
-
                 CollectionBlock block = result.get();
                 block.readData();
                 block.putData(data);
-
+                System.out.println(final_id);
+                System.out.println(block.getId());
+                this.indexManager.getIndex().put(final_id, block.getId());
+                //getIndexManager().addIndex(final_id, block.getId());
             }else{ // otherwise, we create a new block
                 this.currentId++;
                 CollectionBlock block = new CollectionBlock(this.collectionName, this.currentId, this.maxFileSize);
-
                 block.putData(data);
                 blocks.add(block);
-
+                this.indexManager.getIndex().put(final_id, block.getId());
+                //getIndexManager().addIndex(final_id, block.getId());
                 return true;
             }
         } catch (IOException e) {
@@ -80,6 +92,14 @@ public class CougarCollection {
         } catch (IOException e) {
             e.printStackTrace();
         }
+    }
+
+    public CollectionBlock getCollectionBlockByID(int id){
+        return getBlocks().get(id);
+    }
+
+    public TreeMap<UUID, Integer> getIndex(){
+        return this.indexManager.getIndex();
     }
 
     public double getMaxFileSize() {
@@ -136,6 +156,7 @@ public class CougarCollection {
 
     public void loadMap(){
         this.indexManager = new IndexManager(collectionName);
+        System.out.println(collectionName);
         this.blocks = new ArrayList<>();
         for (int i = 0; i <= this.currentId; i++) {
             CollectionBlock block = new CollectionBlock(this.collectionName, i, this.maxFileSize);
